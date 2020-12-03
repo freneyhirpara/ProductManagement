56766071bd12be93b007d5c8a8ae16a1c6331a62
Freney Hirpara <freneyhirpara11@gmail.com>
2020-12-03 12:09:52 +0530

    Initial Solution Setup

---
+++ ProductManagement/ProductManagement.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProductManagement.WebUI.Startup))]
+namespace ProductManagement.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
